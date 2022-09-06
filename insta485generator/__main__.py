a2755485d5d7967191d68f265393ec3644df41d0
patelarj <patelarj@umich.edu>
2022-09-06 14:27:48 -0400

    finished test_hello

--- insta485generator/__main__.py
+++ insta485generator/__main__.py
@@ -1,39 +1,61 @@
 """Build static HTML site from directory of HTML templates and plain files."""
 
-import click;
-import json;
-import jinja2;
+import click
+import json
+import jinja2
+import pathlib
+import os
 
 @click.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('-o','--output',help="Output Directory.",type=click.Path())
 @click.option('-v','--verbose', is_flag=True, help="Print more output.")
 def main(input, output,verbose):
-    print(verbose)
-    
     if not output:
         output = input+'/html'
     else:
         output = output +'/html'
-    print(output)
 
+    q = pathlib.Path(output)
+    if q.exists():
+        print(output, "already exists")
 
-    """if verbose: 
-        print("verbose activated")
-    if output: 
-        print("output activated")
-    if(not verbose and not output):
-        print("error detected, no args")
-
-    template_env = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(str(template_dir)),
-        autoescape=jinja2.select_autoescape(['html', 'xml']),
-    )   
-    template = template_env.get_template()
-    """
+    inptest = pathlib.Path(input)
+    '''if not inptest.exists():
+        print(input, "does not exist") '''
     
-    """Top level command line interface."""
-    print("Usage: insta485generator [OPTIONS] INPUT_DIR \n Try insta485generator --help for help")
+    inpdir = input+"/config.json"
+
+    with open(inpdir, 'r') as t:
+        dicty = json.loads(t.read())
+    
+    tempname = input+'/templates'
+    purename = pathlib.PurePath(tempname)
+    
+    template_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(str(purename)),
+    autoescape=jinja2.select_autoescape(['html', 'xml']),)
+
+    fill = dicty[0]['context']['words']
+    matthog = template_env.get_template('index.html')
+
+    url = dicty[0]['url']
+    url = url.lstrip("/")  # remove leading slash
+    input = pathlib.Path(input)  # convert str to Path object
+    output_dir = input/"html"  # default, can be changed with --output option
+    doscoutput_path = output_dir/url
+
+    
+    #newp = os.path.join(parent,dest)
+    os.mkdir(doscoutput_path)
+    #fulldir = pathlib.Path(parent+"/html/index.html")
+    with open(doscoutput_path/"index.html", 'w', encoding = "utf-8") as f:
+        f.write(matthog.render(words = fill)) 
+
+
+    #print(matthog.render(words = fill))
+
+    #print("Usage: insta485generator [OPTIONS] INPUT_DIR \n Try insta485generator --help for help")
 
 if __name__ == "__main__":
     main()
