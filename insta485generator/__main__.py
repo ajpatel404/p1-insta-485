e7096b37ba4e0540039df86d1eee56c31b4bb855
patelarj <patelarj@umich.edu>
2022-09-07 11:42:29 -0400

    moved to insta485

--- insta485generator/__main__.py
+++ insta485generator/__main__.py
@@ -5,57 +5,81 @@ import json
 import jinja2
 import pathlib
 import os
+import sys
+import shutil
 
 @click.command()
-@click.argument('input', type=click.Path(exists=True))
-@click.option('-o','--output',help="Output Directory.",type=click.Path())
+@click.argument('input_dir', type=click.Path(exists=True))
+@click.option('-o','--output',help="Output directory.",type=click.Path())
 @click.option('-v','--verbose', is_flag=True, help="Print more output.")
-def main(input, output,verbose):
-    if not output:
-        output = input+'/html'
+def main(input_dir, output,verbose):
+    '''Templated static website generator.'''
+    if not output: 
+        output = input_dir+'/html'
+        output = pathlib.Path(output)
+        os.makedirs(output)
     else:
-        output = output +'/html'
-
-    q = pathlib.Path(output)
-    if q.exists():
-        print(output, "already exists")
-
-    inptest = pathlib.Path(input)
-    '''if not inptest.exists():
-        print(input, "does not exist") '''
+        output = pathlib.Path(output)
+        if output.exists():
+            print(output, "already exists")
+            sys.exit(2)
+        else:
+            #output = output/"html"
+            os.makedirs(output)
     
-    inpdir = input+"/config.json"
-
-    with open(inpdir, 'r') as t:
-        dicty = json.loads(t.read())
+    inpdir = pathlib.Path(input_dir + "/config.json")
+    try:
+        with open(inpdir, 'r') as t:
+            try:
+                dicty = json.loads(t.read())
+            except ValueError as err:
+                print("json error detected")
+                sys.exit(2)
+    except FileNotFoundError as err:
+        print("file not found")
+        sys.exit(2)
     
-    tempname = input+'/templates'
-    purename = pathlib.PurePath(tempname)
+    purename = pathlib.PurePath(input_dir+"/templates")
     
-    template_env = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(str(purename)),
-    autoescape=jinja2.select_autoescape(['html', 'xml']),)
+    try:
+        template_env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(str(purename)),
+        autoescape=jinja2.select_autoescape(['html', 'xml']),)
+    except TemplateError:
+        print("Jinja error detected")
+        sys.exit(2)
 
+    for item in dicty:
+        print(item)
+    '''
     fill = dicty[0]['context']['words']
+
     matthog = template_env.get_template('index.html')
 
     url = dicty[0]['url']
     url = url.lstrip("/")  # remove leading slash
-    input = pathlib.Path(input)  # convert str to Path object
-    output_dir = input/"html"  # default, can be changed with --output option
-    doscoutput_path = output_dir/url
+    input_dir = pathlib.Path(input_dir)  # convert str to Path object
+    #output_dir = input_dir/"html"  # default, can be changed with --output option
+    #doscoutput_path = input_dir/"html"
+    doscoutput_path = output/url
 
-    
-    #newp = os.path.join(parent,dest)
-    os.mkdir(doscoutput_path)
-    #fulldir = pathlib.Path(parent+"/html/index.html")
-    with open(doscoutput_path/"index.html", 'w', encoding = "utf-8") as f:
-        f.write(matthog.render(words = fill)) 
+    #print(input_dir)
+
+    if os.path.isdir(input_dir/"static"):
+        os.mkdir(doscoutput_path/"css")
+        shutil.copy(input_dir/"static/css/style.css", doscoutput_path/"css")
+        print("Copied", input_dir/"static", "->", input_dir/"html")
+
+    try:
+        with open(doscoutput_path/"index.html", 'w', encoding = "utf-8") as t:
+            t.write(matthog.render(words = fill)) 
+            print("Rendered index.html ->", doscoutput_path/"index.html")
+    except FileNotFoundError as err:
+        print("file not found")
+        sys.exit(2) '''
 
 
-    #print(matthog.render(words = fill))
 
-    #print("Usage: insta485generator [OPTIONS] INPUT_DIR \n Try insta485generator --help for help")
 
 if __name__ == "__main__":
     main()
